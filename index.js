e9492c4b422ad9b0cc6823a70de0689a1a17f0ab
TheRajath <sairajath7@gmail.com>
2023-03-24 20:21:55 +0530

    Create new product

--- index.js
+++ index.js
@@ -16,12 +16,26 @@ mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/products', async (req, res) => {
 
     const products = await Product.find({});
 
     res.render('products/index', { products });
+});
+
+app.get('/products/new', (req, res) => {
+
+    res.render('products/new');
+});
+
+app.post('/products', async (req, res) => {
+
+    const newProduct = new Product(req.body)
+    await newProduct.save();
+
+    res.redirect(`/products/${newProduct.id}`)
 });
 
 app.get('/products/:id', async (req, res) => {
@@ -35,4 +49,4 @@ app.get('/products/:id', async (req, res) => {
 app.listen(3000, () => {
 
     console.log("APP IS LISTENING ON PORT 3000!");
-})
+});
