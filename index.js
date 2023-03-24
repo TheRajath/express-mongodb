0106d9ef95d525ddbf21fb68b35ea0549ad34f99
TheRajath <sairajath7@gmail.com>
2023-03-24 19:28:45 +0530

    Show all products

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 
-const Product = require('./modules/product');
+const Product = require('./models/product');
 
 mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
     .then(() => {
@@ -17,11 +17,15 @@ mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/dogs', (req, res) => {
-    res.send('WOOF!');
-})
+app.get('/products', async (req, res) => {
+
+    const products = await Product.find({});
+
+    res.render('products/index', { products });
+});
 
 
 app.listen(3000, () => {
+
     console.log("APP IS LISTENING ON PORT 3000!");
 })
