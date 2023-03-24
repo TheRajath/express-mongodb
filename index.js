846ca04362003eef73b27efc9b8cb1c3bbeae91d
TheRajath <sairajath7@gmail.com>
2023-03-24 21:58:26 +0530

    Delete product

--- index.js
+++ index.js
@@ -67,6 +67,13 @@ app.put('/products/:id', async (req, res) => {
     res.redirect(`/products/${product.id}`);
 });
 
+app.delete('/products/:id', async (req, res) => {
+
+    const { id } = req.params;
+    const deleteProduct = await Product.findByIdAndDelete(id);
+    res.redirect('/products');
+});
+
 app.listen(3000, () => {
 
     console.log("APP IS LISTENING ON PORT 3000!");
