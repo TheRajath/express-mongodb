cbc98151f72674a19cc675f96ce7b9b8af427273
TheRajath <sairajath7@gmail.com>
2023-03-24 18:57:47 +0530

    mongoose connect and setup

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
+const express = require('express');
+const app = express();
+const path = require('path');
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/moviesApp')
+
+const Product = require('./modules/product');
+
+mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
     .then(() => {
-        console.log("Connection Open!!!")
+        console.log("MONGO CONNECTION OPEN!!!")
     })
     .catch(err => {
-        console.log("Oh No Error!!!");
+        console.log("OH NO MONGO CONNECTION ERROR!!!");
         console.log(err);
     });
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/dogs', (req, res) => {
+    res.send('WOOF!');
+})
+
+
+app.listen(3000, () => {
+    console.log("APP IS LISTENING ON PORT 3000!");
+})
