be11f5cc5d89512c83084d5d22d36935b1db815b
TheRajath <sairajath7@gmail.com>
2023-03-25 22:17:29 +0530

    handle Validation Error

--- index.js
+++ index.js
@@ -115,6 +115,21 @@ app.delete('/products/:id', wrapAsync(async (req, res) => {
     res.redirect('/products');
 }));
 
+const handleValidationError = err => {
+
+    console.log(err);
+    return new AppError(`Validation Failed...${err.message}`, 400);
+}
+
+app.use((err, req, res, next) => {
+
+    console.log(err.name);
+
+    if (err.name === 'ValidationError') err = handleValidationError(err);
+
+    next(err);
+});
+
 app.use((err, req, res, next) => {
 
     const { status = 500, message = 'Something went wrong' } = err;
