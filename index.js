4f803727bf8e085185d72f1392bfa9a636bbd478
TheRajath <sairajath7@gmail.com>
2023-03-26 16:05:45 +0530

    show page for farms

--- index.js
+++ index.js
@@ -53,6 +53,13 @@ app.post('/farms', async (req, res) => {
     res.redirect('/farms');
 });
 
+app.get('/farms/:id', async (req, res) => {
+
+    const farm = await Farm.findById(req.params.id);
+
+    res.render('farms/show', { farm });
+});
+
 // PRODUCT ROUTES
 app.get('/products', wrapAsync(async (req, res, next) => {
 
