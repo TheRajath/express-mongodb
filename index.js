c01ddd0fd970c6b85ac6b38e2936be4f9d6eb481
TheRajath <48343927+TheRajath@users.noreply.github.com>
2023-03-26 17:07:43 +0530

    Add farm using mongoose relationship (#4)
    * create farm model
    
    * add farms
    
    * show page for farms
    
    * add new farm and link it to products
    
    * delete a farm and all objects linked to it
    
    * add new farm link in index page

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const methodOverride = require('method-override');
 const AppError = require('./AppError');
 
 const Product = require('./models/product');
+const Farm = require('./models/farm');
 
 mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
     .then(() => {
@@ -31,6 +32,67 @@ function wrapAsync(fn) {
     }
 }
 
+// FARM ROUTES
+app.get('/farms', async (req, res) => {
+
+    const farms = await Farm.find({});
+
+    res.render('farms/index', { farms });
+});
+
+app.get('/farms/new', (req, res) => {
+
+    res.render('farms/new');
+});
+
+app.post('/farms', async (req, res) => {
+
+    const farm = new Farm(req.body);
+    await farm.save();
+
+    res.redirect('/farms');
+});
+
+app.get('/farms/:id', async (req, res) => {
+
+    const farm = await Farm.findById(req.params.id).populate('products');
+
+    res.render('farms/show', { farm });
+});
+
+app.delete('/farms/:id', async (req, res) => {
+
+    const farm = await Farm.findByIdAndDelete(req.params.id);
+
+    res.redirect('/farms');
+});
+
+app.get('/farms/:id/products/new', async (req, res) => {
+
+    const { id } = req.params;
+    const farm = await Farm.findById(id);
+
+    res.render('products/new', { categories, farm });
+});
+
+app.post('/farms/:id/products', async (req, res) => {
+
+    const { id } = req.params;
+    const farm = await Farm.findById(id);
+
+    const { name, price, category } = req.body;
+    const product = new Product({ name, price, category });
+
+    farm.products.push(product);
+    product.farm = farm;
+
+    await farm.save();
+    await product.save();
+
+    res.redirect(`/farms/${farm._id}`);
+});
+
+// PRODUCT ROUTES
 app.get('/products', wrapAsync(async (req, res, next) => {
 
     const { category } = req.query;
@@ -69,7 +131,7 @@ app.get('/products/:id', wrapAsync(async (req, res, next) => {
         throw new AppError('Invalid Id', 400);
     }
 
-    const product = await Product.findById(id);
+    const product = await Product.findById(id).populate('farm', 'name');
 
     if (!product) {
 
